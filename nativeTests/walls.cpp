e34e124cf809a0f876570c4102eb20b9c4a963f8
root <root@Omega-46C1.lan>
2018-07-04 08:45:44 +0000

    START to design 'walls' nativeAnimatedBackground

---
+++ nativeTests/walls.cpp
@@ -0,0 +1,17 @@
+/**
+
+  32 chars, 0terminated
+  
+  1+1+30
+  \ \  \_ 0-240 wall height + 0-15 wall attribs (color)
+   \ \___ ground attribs  
+    \____ sky attribs
+*/
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+
+ printf("Hello\n");
+
+}
