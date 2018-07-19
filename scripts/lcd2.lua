535ab5bf44bb93a472abd96be76033111f49f911
root <root@Omega-46C1.lan>
2018-07-19 19:14:24 +0000

    LCD Sample now uses NativeAnimatedBackgrounds

--- scripts/lcd2.lua
+++ scripts/lcd2.lua
@@ -17,9 +17,9 @@ lcdrect( 10, 10, 128, 64, 0, 6 )
 
 delay(1000)
 
-print("A.2\n")
-dofile("scripts/starfieldFX.lua")
-print("A.3\n")
+--print("A.2\n")
+--dofile("scripts/starfieldFX.lua")
+--print("A.3\n")
 
 local e
 e=1
@@ -28,14 +28,28 @@ print("A.4\n")
 print("A.5\n")
 
 lcdcls()
+while e <= 40 do
+--print("A.5.1\n")
 
+--lcdblitt(0)
+--  lcdcls() 
+  lcdbackg(1, 45+e, 90, 45+e, 6); -- draw "Balls", GREEN
+--lcdblitt(2)
+
+  e = e+1
+end
+
+
+e=1
+lcdcls()
 while e <= 100 do
 --print("A.5.1\n")
 
-lcdblitt(0)
-  lcdcls() 
-  drawStarFieldFrame(e)
-lcdblitt(2)
+--lcdblitt(0)
+--  lcdcls() 
+  --drawStarFieldFrame(e)
+  lcdbackg(2, 50, 100, 1, 8); -- draw "Starfield", 50starts, 100% speed, 1, PINK
+--lcdblitt(2)
 
   e = e+1
 end
